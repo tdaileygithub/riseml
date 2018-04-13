441e93da5b37d3339e130e26e414a6ede8ef73ea
elmarhaussmann <elmar@riseml.com>
2018-04-13 18:28:46 +0800

    Add copyright

--- docs/deploy.sh
+++ docs/deploy.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 # "Deploys" the current version of our docs on S3
-BUILD_FOLDER=_book
+BUILD_FOLDER=site
 S3_BUCKET=s3://docs.riseml.com/
 
+cd ..
 # Clean local build
 rm -r $BUILD_FOLDER
 # Create local repository folder (if not present)
 mkdir -p $BUILD_FOLDER
 
 # Create package
-gitbook build
+mkdocs build -f docs/mkdocs.yml -d $BUILD_FOLDER
+# Remove artifacts that don' belong deployed
+rm $BUILD_FOLDER/mkdocs.yml
+rm $BUILD_FOLDER/requirements.txt
 
 # Sync local build folder > remote s3 bucket, delete remote files not present locally
-aws s3 sync --delete $BUILD_FOLDER $S3_BUCKET
+aws s3 sync --delete $BUILD_FOLDER $S3_BUCKET --dryrun
 
 # Invalidate CloudFront CDN
 aws configure set preview.cloudfront true
 aws cloudfront create-invalidation --distribution-id E18Z09OA3VI8UF --paths "/*"
-
+cd docs
